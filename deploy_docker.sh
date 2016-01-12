52f0808d4519f18778bd66ac19e48fbe1fdff90a
rigel <rigel.k@kakaocorp.com>
2016-01-12 14:09:14 +0900

    Fix env  add tomcat dependency

--- deploy_docker.sh
+++ deploy_docker.sh
@@ -7,8 +7,8 @@ USER=567233e54423f8a8ca001d3c
 SERVER=sun-dubu.cloudsc.kr
 WEB_HOME=/var/lib/openshift/567233e54423f8a8ca001d3c/egovframework/webapps
 
-#git pull
-#mvn clean package -Dmaven.test.skip=true
+git pull
+mvn clean package -Dmaven.test.skip=true
 
 SSH="ssh -o StrictHostKeyChecking=no -i ~/.ssh/id_open"
 
