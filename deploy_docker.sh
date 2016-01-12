bf6c8992aa81d10d24ff0ba1a0b9a44acd8d66a1
rigel <rigel.k@kakaocorp.com>
2016-01-12 13:20:36 +0900

    Fix env

--- deploy_docker.sh
+++ deploy_docker.sh
@@ -1,30 +1,19 @@
 #!/bin/bash
 
-APP_NAME=rigel/service
-APP_DIR=/home
-REVISION=0.3
-CONTAINER_NAME=service
-REGISTRY=idock.daumkakao.io
+
+#ssh -i ~/.ssh/id_open 567233e54423f8a8ca001d3c@sun-dubu.cloudsc.kr
+#/var/lib/openshift/567233e54423f8a8ca001d3c/egovframework/webapps
+
+USER=567233e54423f8a8ca001d3c
+SERVER=sun-dubu.cloudsc.kr
+WEB_HOME=/var/lib/openshift/567233e54423f8a8ca001d3c/egovframework/webapps
 
 git pull
-mvn clean package -Dmaven.test.skip=true
-scp target/springboot-shop.war deploy@d06.s1.krane.9rum.cc:/home/deploy
+mvn clean package
 
-APP_SERVERS=(
-  "deploy@d06.s1.krane.9rum.cc"
-)
+SSH="ssh -o StrictHostKeyChecking=no -i ~/.ssh/id_open"
+SCP="scp -i ~/.ssh/id_open"
 
-#GIT_WORK_TREE=$APP_DIR git checkout -f
-
-SSH="ssh -o StrictHostKeyChecking=no"
-for SERVER in ${APP_SERVERS[@]}
-do
-  # Delete all containers
-  # $SSH $SERVER docker rm $(docker ps -a -q)
-  # Delete all images
-  # $SSH $SERVER docker rmi $(docker images -q)
-
-  $SSH $SERVER docker cp springboot-shop.war $CONTAINER_NAME:/home/nobody/tomcat/apache-tomcat-8.0.28/webapps/ROOT.war
-  $SSH $SERVER docker exec $CONTAINER_NAME /home/nobody/tomcat/apache-tomcat-8.0.28/bin/shutdown.sh
-  $SSH $SERVER docker exec $CONTAINER_NAME /home/nobody/tomcat/apache-tomcat-8.0.28/bin/startup.sh
-done
+$SCP target/springboot-shop.war $USER@$SERVER:$WEB_HOME/ROOT.war
+$SSH $USER@$SERVER gear stop
+$SSH $USER@$SERVER gear start
