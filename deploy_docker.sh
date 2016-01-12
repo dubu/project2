ccfdb5a942146ca4b65682546e81b3a0fbe245a1
rigel <rigel.k@kakaocorp.com>
2016-01-12 13:51:13 +0900

    Fix env

--- deploy_docker.sh
+++ deploy_docker.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 #ssh -i ~/.ssh/id_open 567233e54423f8a8ca001d3c@sun-dubu.cloudsc.kr
 #/var/lib/openshift/567233e54423f8a8ca001d3c/egovframework/webapps
 
@@ -8,12 +7,12 @@ USER=567233e54423f8a8ca001d3c
 SERVER=sun-dubu.cloudsc.kr
 WEB_HOME=/var/lib/openshift/567233e54423f8a8ca001d3c/egovframework/webapps
 
-git pull
-mvn clean package
+#git pull
+#mvn clean package -Dmaven.test.skip=true
 
 SSH="ssh -o StrictHostKeyChecking=no -i ~/.ssh/id_open"
-SCP="scp -i ~/.ssh/id_open"
 
-$SCP target/springboot-shop.war $USER@$SERVER:$WEB_HOME/ROOT.war
-$SSH $USER@$SERVER gear stop
-$SSH $USER@$SERVER gear start
+scp -i ~/.ssh/id_open target/springboot-shop.war $USER@$SERVER:$WEB_HOME/ROOT.war
+
+ssh -i ~/.ssh/id_open $USER@$SERVER gear stop
+ssh -i ~/.ssh/id_open $USER@$SERVER gear start
